bab78ec129a6812a626d6f46b70eaf8a943ee85b
MuhasinBiruar <mxebiruar@addu.edu.ph>
2026-01-10 03:38:32 +0800

    added profile card

--- app/components/headerfooter.jsx
+++ app/components/headerfooter.jsx
@@ -4,15 +4,16 @@ export default function Header() {
   return (
     <>
       {/* This is our new header */}
-      <div className="pt-2 pb-2 sticky top-0 border-b border-t bg-gray-700 text-white shadow-2xl"> 
+      <div className="pt-2 pb-2 sticky top-0 border-b border-t bg-gray-700 text-white shadow-2xl "> 
         <div className="flex justify-between"> {/* This is for fixing and positioning*/}
           <div className="relative top-1 ml-4"> {/* Spacing and margin*/}
-            <span className="text-2xl font-bold">Dashboard</span>
+            <span className="text-2xl font-bold font-serif">Dashboard</span>
           </div>
-          <nav className=" mr-4 text-lg pt-1 pb-1 gap-2 mt-1 mb-1">
-            <Link className="p-2 rounded mr-2 hover:bg-blue-500 hover:text-white" href="/">Home</Link>
-            <Link className="p-2 rounded mr-2 hover:bg-blue-500 hover:text-white" href="/about">About</Link>
-            <Link className="p-2 rounded mr-2 hover:bg-blue-500 hover:text-white" href="/contact">Contact</Link>
+          <nav className=" mr-4 text-lg pt-1 pb-1 gap-2 mt-1 mb-1 transition-transform duration-300 ease-in-out 
+              hover:scale-110">
+            <Link className="p-2 rounded mr-2 hover:bg-orange-500 font-serif" href="/">Home</Link>
+            <Link className="p-2 rounded mr-2 hover:bg-orange-500 font-serif" href="/about">About</Link>
+            <Link className="p-2 rounded mr-2 hover:bg-orange-500 font-serif" href="/contact">Contact</Link>
           </nav>
         </div>
       </div>
@@ -25,7 +26,7 @@ export function Footer() {
   return (
     <>
       {/* This is our new header */}
-      <div className="fixed bottom-0 w-full border-b border-t bg-gray-700 text-white text-center shadow-2xl"> 
+      <div className="fixed bottom-0 w-full border-b border-t bg-gray-700 text-white text-center shadow-2xl font-serif"> 
         This is the footer
       </div>
       {/* End of Footer and start of the main div */}
--- app/page.jsx
+++ app/page.jsx
@@ -2,8 +2,12 @@
 
 export default function Home() {
   return (
-    <div className="text-center">
-      <h2>Muhaisin Xavier E. Biruar</h2>
+    <div className="p-8">
+      <div className=" bg-gray-600 rounded-lg shadow-2xl p-8 max-w-62 transition-transform duration-300 ease-in-out 
+              hover:scale-110">
+        <img src="/s7xn4xUQ.jpg" alt="profile picture" className="ml-3 w-40 rounded-full shadow-xl shadow-orange-300 border-2 border-amber-200"></img>
+        <h2 className="mt-8 text-center font-serif text-orange-300">Muhaisin Xavier E. Biruar</h2>
+      </div>
     </div>
   );
 }
