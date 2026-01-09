9b95091ef9563dcd88ba7f8e7ae608b51d408f69
MuhasinBiruar <mxebiruar@addu.edu.ph>
2026-01-10 00:45:59 +0800

    globals and headerfooter changes

--- app/components/headerfooter.jsx
+++ app/components/headerfooter.jsx
@@ -4,12 +4,12 @@ export default function Header() {
   return (
     <>
       {/* This is our new header */}
-      <div className="pt-2 pb-2 sticky top-0 border-b border-t bg-gray-700 text-white"> 
+      <div className="pt-2 pb-2 sticky top-0 border-b border-t bg-gray-700 text-white shadow-2xl"> 
         <div className="flex justify-between"> {/* This is for fixing and positioning*/}
           <div className="relative top-1 ml-4"> {/* Spacing and margin*/}
             <span className="text-2xl font-bold">Dashboard</span>
           </div>
-          <nav className="mr-4 text-lg pt-1 pb-1 gap-2 mt-1 mb-1">
+          <nav className=" mr-4 text-lg pt-1 pb-1 gap-2 mt-1 mb-1">
             <Link className="p-2 rounded mr-2 hover:bg-blue-500 hover:text-white" href="/">Home</Link>
             <Link className="p-2 rounded mr-2 hover:bg-blue-500 hover:text-white" href="/about">About</Link>
             <Link className="p-2 rounded mr-2 hover:bg-blue-500 hover:text-white" href="/contact">Contact</Link>
@@ -25,7 +25,7 @@ export function Footer() {
   return (
     <>
       {/* This is our new header */}
-      <div className="fixed bottom-0 w-full border-b border-t bg-gray-700 text-white text-center"> 
+      <div className="fixed bottom-0 w-full border-b border-t bg-gray-700 text-white text-center shadow-2xl"> 
         This is the footer
       </div>
       {/* End of Footer and start of the main div */}
