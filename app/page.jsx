799cb310f4a8310671a38d08605c6c735138e8fd
MuhasinBiruar <mxebiruar@addu.edu.ph>
2026-01-10 22:22:15 +0800

    fixed some responsiveness for smaller screens

--- app/page.jsx
+++ app/page.jsx
@@ -27,21 +27,21 @@ export default function Home() {
       <div className="p-8 pl-30 pt-0 w-full h-full md:w-[85%]">
         <h2 className="font-serif font-bold text-left text-lg">About me</h2>
         <hr className="mt-2 "/>
-        <div className="h-full mt-4 p-4 pt-2 bg-white md:h-[40%] rounded-md border-2 
-        border-gray-300 shadow-lg transition-transform duration-300 ease-in-out hover:scale-105">
-          <p className="font-bold font-serif text-left text-md">Student</p>
+        <div className="mt-4 p-3 pt-2 md:p-4 md:pt-2 bg-white h-auto md:h-[40%] rounded-md border-2 
+        border-gray-300 shadow-lg transition-transform duration-300 ease-in-out hover:scale-105 overflow-auto w-full">
+          <p className="font-bold font-serif text-left text-sm md:text-md">Student</p>
           <p className="font-serif text-left text-xs text-gray-400">March 16, 2005</p>
-          <p className="font-serif text-left text-sm mt-1">Currently studying in <b>Ateneo de Davao University for a Bachelor's 
+          <p className="font-serif text-left text-xs md:text-sm mt-1">Currently studying in <b>Ateneo de Davao University for a Bachelor's 
             degree in BS Computer Science, </b>
             and is expected to graduate in <b>2028.</b> Likes to watch movies, anime, and play video games on spare time. Started to gain
             knowledge and experience in coding during the pandemic era. Realized that this is the path to pursue in college. Still on the road
             to learn branches of Computer Science. Currently not too sure on which specific area to specialize in.</p>
-          <p className="font-serif text left text-sm mt-2">As of the moment, specific interests <b>include AWS, cybersecurity, web development, software
+          <p className="font-serif text-left text-xs md:text-sm mt-2">As of the moment, specific interests <b>include AWS, cybersecurity, web development, software
             engineering, artificial intelligence, and machine learning.</b> Looking forward to explore the vast field of Computer Science and finally
             find the right specialization to focus on in the future. Currently enhancing skills in web development using <b>Next.js and React.js</b> frameworks
             as part of school projects. Hoping to gain more experience and knowledge to get ready for the industry after graduation.
           </p>
-          <p className="font-serif text-left text-sm mt-2">Skills include some proficiency with <b>java, javascript, c, c++, python, 
+          <p className="font-serif text-left text-xs md:text-sm mt-2">Skills include some proficiency with <b>java, javascript, c, c++, python, 
             css, html, react.js, next.js, mysql, postgresql and basic understanding of AWS services.</b> This also includes knowledge in <b> OOP concepts, 
             data structures, algorithms, web development, and database management.</b>
           </p>
