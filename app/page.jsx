c642b584c48281a6f483a99e4424bd87188d13e3
MuhasnBiruare <mxebiruar@addu.edu.ph>
2026-01-12 18:40:22 +0800

    Optimized readability and responsiveness

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,8 @@
 import Image from "next/image";
-import { School, Notebook } from 'lucide-react';
+import { School, Notebook, Mail } from 'lucide-react';
+import Animation, {divGap} from "./components/utility.jsx";
+
+const animate = "transition-transform duration-300 ease-in-out hover:scale-115";
 
 export default function Home() {
   return (
@@ -11,10 +14,12 @@ export default function Home() {
          shadow-orange-300 border-2 border-amber-200 transition-transform duration-300 ease-in-out hover:scale-120" />
           <h2 className="mt-8 text-center font-serif text-orange-300">Muhaisin Xavier E. Biruar</h2>
         </div>
-        <div className="pt-2 pl-2 w-full h-[20%] bg-gray-600 rounded-lg mt-8 transition-transform duration-300 ease-in-out
-        hover:scale-110 shadow-2xl border-2 border-gray-400 overflow-auto">
+        <div className={`pt-2 pl-2 w-full h-[20%] bg-gray-600 rounded-lg mt-8 {} ${Animation.hover(110)} shadow-2xl border-2 border-gray-400 overflow-auto`}>
             <p className="text-left text-orange-300 font-serif text-sm">Computer Science Student / 2nd year</p>
-            <p className="text-left text-orange-300 font-serif text-sm">20 Years Old</p>
+            <div className={`${divGap(2)}`}>
+            <Mail size={12}color="orange" strokeWidth={2.75} className={Animation.hover(110)}/>
+            <p className="text-left text-orange-300 font-serif text-sm">mxebiruar@addu.edu.ph</p>
+            </div>
             <p className="text-left text-orange-300 font-serif text-sm">Davao City, Philippines</p>
             <p className="text-left text-orange-300 font-serif text-sm hover:text-blue-500 underline">
               <a href="https://www.facebook.com/hyder3162005" target="_blank">Facebook</a></p>
@@ -28,10 +33,10 @@ export default function Home() {
       <div className="p-8 pl-30 mt-5 ml-[-20%] md:ml-0 md:mt-0 pt-0 w-[120%] h-full md:w-[85%]">
         <h2 className="font-serif font-bold text-left text-lg">About me</h2>
         <hr className="mt-2 "/>
-        <div className="mt-4 p-3 pt-2 md:p-4 md:pt-2 bg-white h-auto md:h-[40%] rounded-md border-2 
-        border-gray-300 shadow-lg transition-transform duration-300 ease-in-out hover:scale-105 overflow-auto w-full">
+        <div className={`mt-4 p-3 pt-2 md:p-4 md:pt-2 bg-white h-auto md:h-[40%] rounded-md border-2 
+        border-gray-300 shadow-lg ${Animation.hover(110)} overflow-auto w-full`}>
           <div className="flex items-center gap-2">
-            <Notebook size={24} color="Orange"/>
+            <Notebook size={24} color="Orange" className={Animation.hover(110)}/>
             <p className="font-bold font-serif text-left text-sm md:text-md">Student</p>
           </div>
           <p className="font-serif text-left text-xs text-gray-400">March 16, 2005</p>
@@ -51,10 +56,10 @@ export default function Home() {
           </p>
         </div>
         <div className="flex flex-row justify gap-4 h-[45%] mt-4">
-          <div className="bg-white rounded-md border-2 border-gray-300 shadow-lg p-2 h-full w-[50%]
-          transition-transform duration-300 ease-in-out hover:scale-105 overflow-auto">
+          <div className={`bg-white rounded-md border-2 border-gray-300 shadow-lg p-2 h-full w-[50%]
+          ${Animation.hover(105)} overflow-auto`}>
             <div className="flex items-center gap-2">
-              <School size={24} color="Orange"/>
+              <School size={24} color="Orange" className={Animation.hover(110)}/>
               <p className="font-bold font-serif text-left text-md">
                 Academic Journey
               </p>
@@ -62,16 +67,16 @@ export default function Home() {
             <ul className="list-disc list-inside ml-2">
               <li className="font-serif text-xs md:text-sm mt-1 flex justify-between items-center">
                 <span className="list-item"><b>Cotabato City Institute</b> (Kindergarten)</span>
-                <span className="text-gray-400 font-bold">2011-2013</span>
+                <span className={`text-gray-400 font-bold ${Animation.hover(115)}`}>2011-2013</span>
               </li>
               <li className="font-serif text-xs md:text-sm mt-1 flex justify-between items-center">
                 <span className="list-item"><b>Ateneo de Davao University Senior High School</b> (Senior High)</span>
-                <span className="text-gray-400 font-bold">2021-2023</span>
+                <span className={`text-gray-400 font-bold ${Animation.hover(115)}`}>2021-2023</span>
               </li>
             </ul>
           </div>
-          <div className="bg-white rounded-md border-2 border-gray-300 shadow-lg p-2 h-full w-[50%]
-          transition-transform duration-300 ease-in-out hover:scale-105">
+          <div className={`bg-white rounded-md border-2 border-gray-300 shadow-lg p-2 h-full w-[50%]
+          ${Animation.hover(105)} overflow-auto`}>
             <p className="font-bold font-serif text-left text-md">
               Beepes2
             </p>
