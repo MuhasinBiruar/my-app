f1862d1e024ab054724bce22528b09c708fa3f70
MuhasnBiruare <mxebiruar@addu.edu.ph>
2026-01-16 13:21:14 +0800

    Added Album

--- app/dashboard/modals/modals.tsx
+++ app/dashboard/modals/modals.tsx
@@ -27,6 +27,7 @@ export default function Modals() {
     const [isAnimating, setIsAnimating] = useState(false);
     const relatedPictures = SCHOOLPICTURES.filter((picture) => picture.category === selectedPicture?.category && picture.id !== selectedPicture?.id
     );
+    const [reset, setReset] = useState(false);
 
     useEffect(() => {
         if (selectedPicture) {
@@ -77,16 +78,38 @@ export default function Modals() {
                 transition-opacity duration-300 ${isAnimating ? 'opacity-100' : 'opacity-0'}`}
                 onClick={handleClose}>
                     <div className={`bg-white rounded-2xl drop-shadow-black drop-shadow-xl border border-black p-4
-                    w-[75%] h-[75%] mx-4 relative overflow-hidden flex flex-col 
-                    md:flex-row transition-transform duration-300 ${isAnimating ? 'scale-100' : 'scale-95'} justify-between`} 
+                    w-[90%] h-[75%] mx-4 relative overflow-hidden flex flex-col 
+                    md:flex-row transition-transform duration-300 ${isAnimating ? 'scale-100' : 'scale-95'} 
+                    justify-between gap-2`} 
                     onClick={(e) => e.stopPropagation()}>
                         <button className="hover:bg-red-500 rounded-full 
-                        transition-colors duration-200 ease-in-out absolute top-2 right-2 text-gray-600 hover:text-gray-800 z-10 text-2xl font-bold px-3 py-1" 
+                        transition-colors duration-200 ease-in-out absolute top-2 right-2 text-gray-600 hover:text-gray-800 
+                        z-10 text-2xl font-bold px-3 py-1 sticky-top" 
                                 onClick={handleClose}>
                             ✕
                         </button>
-                        <div className="bg-black h-[50%] w-[50%]">
-                            peanits
+                        <div className="bg-white h-[50%] w-[75%] mt-10 rounded-xl overflow-hidden">
+                            <img src={selectedPicture.src} alt={selectedPicture.title} className="w-full h-full object-cover rounded-xl
+                            transition-all duration-1000 hover:rotate-3600 hover:scale-1 hover:drop-shadow-black hover:drop-shadow-xl"/>
+                        </div>
+                        <div className="flex flex-row gap-2 w-full mt-10 h-full overflow-auto">
+                        {SCHOOLPICTURES.filter(pic => pic.id !== selectedPicture?.id).reduce((acc, pic, index, array) => {
+                            if (index % 2 === 0) {
+                                acc.push(array.slice(index, index + 2));
+                            }
+                            return acc;
+                        }, [] as schoolPicture[][]).map((column, colIndex) => (
+                            <div key={colIndex} className="flex flex-col gap-1 w-[50%] h-[50%] ">
+                                {column.map((pic) => (
+                                    <div key={pic.id} className="w-full h-[50%] rounded-xl hover:z-50 ">
+                                        <img src={pic.src} alt={pic.title} className="w-full h-full object-cover rounded-xl 
+                                        transition-all duration-400 hover:scale-110 hover:drop-shadow-xl hover:drop-shadow-black
+                                        hover:border hover:border-black" onClick={() => setSelectedPicture(pic)}
+                                         />
+                                    </div>
+                                ))}
+                            </div>
+                        ))}
                         </div>
                     </div>
                 </div>,
