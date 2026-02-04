e24d14e1beb6e20c7adbfc224c570def2e835725
MuhasnBiruare <mxebiruar@addu.edu.ph>
2026-02-04 12:05:44 +0800

    added responsiveness to about

--- app/dashboard/about/page.tsx
+++ app/dashboard/about/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
       profilePicDivClassName="bg-gray-600/600! border-0! w-full! shadow-orange-300/300! hover:scale-100! mt-10!"
       picDivClassName="w-full md:w-80! border-2! border-black! shadow-black! shadow-lg!"
       h2ClassName="mt-15! text-black! font-bold! text-3xl!"
-      profContactClassName="bg-gray-300/300! border-0! shadow-orange-300/300! flex! flex-col! gap-2! items-center! justify-center! hover:scale-100! overflow-visible!"
+      profContactClassName="bg-gray-300/300! h-full! md:h-35! border-0! shadow-orange-300/300! flex! flex-col! gap-2! items-center! justify-center! hover:scale-100! overflow-visible!"
       profPClassName="text-black! font-bold! text-center! text-lg!"
       profColor="black"
       />
